d121edc4b16ef4d287db260d5132a69843fa3bab
Владимир Солдатов <cbb110@rambler.ru>
2022-12-21 18:21:11 +0300

    test

---
+++ ConnectDB/MainForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace ConnectDB
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.вывестиПервые10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 612);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1023, 181);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 517);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 517);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(308, 517);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 549);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Название процедуры";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 549);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Текст процедуры";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 457);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 32);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 464);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(164, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(308, 464);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(164, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 397);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 32);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(308, 404);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(164, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(123, 404);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(164, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 487);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Текст процедуры";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 487);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Название процедуры";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(345, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Текст процедуры";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(149, 438);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Название процедуры";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вывестиПервые10ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 26);
+            // 
+            // вывестиПервые10ToolStripMenuItem
+            // 
+            this.вывестиПервые10ToolStripMenuItem.Name = "вывестиПервые10ToolStripMenuItem";
+            this.вывестиПервые10ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.вывестиПервые10ToolStripMenuItem.Text = "Вывести первые10";
+       
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 181);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(1023, 194);
+            this.dataGridView2.TabIndex = 18;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 671);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem вывестиПервые10ToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ ConnectDB/MainForm.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConnectDB
+{
+    public partial class MainForm : Form
+    {
+        SqlConnection sqlConnection = null;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                sqlConnection = new SqlConnection();
+                {
+                    sqlConnection.ConnectionString = "Server=localhost; Initial Catalog=webpbxReportDB; Integrated Security=SSPI;";
+                    sqlConnection.Open();
+
+                    MessageBox.Show("Connected");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection == null)
+                button1.PerformClick();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "" +
+                "select name, object_definition(object_id) " +
+                "from sys.procedures ";
+            if (!String.IsNullOrEmpty(textBox1.Text) || !String.IsNullOrEmpty(textBox2.Text))
+            {
+                sqlCommand.CommandText += "where ";
+            }
+            if (!String.IsNullOrEmpty(textBox1.Text) && String.IsNullOrEmpty(textBox2.Text))
+            {
+                sqlCommand.CommandText += $"name={textBox1.Text} ";
+            }
+            if (!String.IsNullOrEmpty(textBox1.Text) && !String.IsNullOrEmpty(textBox2.Text))
+            {
+                sqlCommand.CommandText += $"name={textBox1.Text} and object_definition(object_id)  like '{textBox2.Text}'";
+            }
+            if (String.IsNullOrEmpty(textBox1.Text) && !String.IsNullOrEmpty(textBox2.Text))
+            {
+                sqlCommand.CommandText += $"object_definition(object_id)  like '%{textBox2.Text}%'";
+            }
+            MessageBox.Show(sqlCommand.CommandText);
+            try
+            {
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+
+                    List<string> data = new List<string>();
+
+                    for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        dataGridView1.Columns.Add(sqlDataReader.GetName(i), sqlDataReader.GetName(i));
+
+
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                data.Add(sqlDataReader.GetValue(i).ToString());
+                            }
+                            dataGridView1.Rows.Add(data.ToArray());
+                            data.Clear();
+                        }
+                    }
+                    dataGridView1.AutoResizeColumns();
+                    dataGridView1.Width = 0;
+                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                    {
+
+                        dataGridView1.Columns[i].Width = dataGridView1.Columns[0].Width;
+                        dataGridView1.Width += dataGridView1.Columns[0].Width;
+
+                    }
+
+                    if (this.Width > 453)
+                        this.Width = dataGridView1.Width + 100;
+                    else
+                        this.Width = 463;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " \n " + ex.HResult + " \n " + ex.Source + " \n " + ex.StackTrace + " \n " + ex.HelpLink + " \n " + ex.HelpLink);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection == null)
+                button1.PerformClick();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "" +
+                "SELECT Table_Catalog as[Имя БД], table_SCHEMA as [Имя схемы], TABLE_NAME AS[Имя таблицы] " +
+                "FROM INFORMATION_SCHEMA.TABLES " +
+                "WHERE table_type = 'BASE TABLE' ";
+            if (!String.IsNullOrEmpty(textBox3.Text) || !String.IsNullOrEmpty(textBox4.Text))
+            {
+                sqlCommand.CommandText += "where ";
+            }
+            if (!String.IsNullOrEmpty(textBox3.Text) && String.IsNullOrEmpty(textBox4.Text))
+            {
+                sqlCommand.CommandText += $"[Имя таблицы]={textBox1.Text} ";
+            }
+            if (!String.IsNullOrEmpty(textBox3.Text) && !String.IsNullOrEmpty(textBox4.Text))
+            {
+                sqlCommand.CommandText += $"[Имя таблицы]={textBox1.Text} and [Имя схемы]  like '{textBox4.Text}'";
+            }
+            if (String.IsNullOrEmpty(textBox3.Text) && !String.IsNullOrEmpty(textBox4.Text))
+            {
+                sqlCommand.CommandText += $"[Имя схемы]  like '%{textBox4.Text}%'";
+            }
+            sqlCommand.CommandText += " order by [Имя схемы], [Имя таблицы]";
+            MessageBox.Show(sqlCommand.CommandText);
+            try
+            {
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+
+                    List<string> data = new List<string>();
+
+                    for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        dataGridView1.Columns.Add(sqlDataReader.GetName(i), sqlDataReader.GetName(i));
+
+
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                data.Add(sqlDataReader.GetValue(i).ToString());
+                            }
+                            dataGridView1.Rows.Add(data.ToArray());
+                            data.Clear();
+                        }
+                    }
+                    dataGridView1.AutoResizeColumns();
+                    dataGridView1.Width = 0;
+                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                    {
+
+
+                        dataGridView1.Width += dataGridView1.Columns[i].Width + 10;
+
+                    }
+
+                    if (this.Width > 500)
+                        this.Width = dataGridView1.Width + 100;
+                    else
+                        this.Width = 500;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " \n " + ex.HResult + " \n " + ex.Source + " \n " + ex.StackTrace + " \n " + ex.HelpLink + " \n " + ex.HelpLink);
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection == null)
+                button1.PerformClick();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+            sqlCommand.CommandText = "" +
+                  "SELECT Table_Catalog as[Имя БД], table_SCHEMA as [Имя схемы], TABLE_NAME AS[Имя таблицы], " +
+                  "COLUMN_NAME AS[Имя столбца], " +
+                  "DATA_TYPE AS[Тип данных столбца], " +
+                  "IS_NULLABLE AS[Значения NULL] " +
+                  "FROM INFORMATION_SCHEMA.COLUMNS ";
+            if (!String.IsNullOrEmpty(textBox5.Text) || !String.IsNullOrEmpty(textBox6.Text))
+            {
+                sqlCommand.CommandText += "where ";
+                if (!String.IsNullOrEmpty(textBox5.Text) && String.IsNullOrEmpty(textBox6.Text))
+                {
+                    sqlCommand.CommandText += $"Table_Name like '%{textBox5.Text}%' ";
+                    sqlCommand.CommandText += " order by Table_Name";
+                }
+                if (!String.IsNullOrEmpty(textBox5.Text) && !String.IsNullOrEmpty(textBox6.Text))
+                {
+                    sqlCommand.CommandText += $"Table_Name like '%{textBox5.Text}%' and Column_Name  like '%{textBox6.Text}%'";
+                    sqlCommand.CommandText += " order by Table_name, Column_Name";
+                }
+                if (String.IsNullOrEmpty(textBox5.Text) && !String.IsNullOrEmpty(textBox6.Text))
+                {
+                    sqlCommand.CommandText += $"Column_Name  like '%{textBox6.Text}%'";
+                    sqlCommand.CommandText += " order by Column_Name";
+                }
+            }
+            else
+                sqlCommand.CommandText += "order by Table_Name, Column_Name";
+            try
+            {
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Columns.Clear();
+
+                    List<string> data = new List<string>();
+
+                    for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        dataGridView1.Columns.Add(sqlDataReader.GetName(i), sqlDataReader.GetName(i));
+
+
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                data.Add(sqlDataReader.GetValue(i).ToString());
+                            }
+                            dataGridView1.Rows.Add(data.ToArray());
+                            data.Clear();
+                        }
+                    }
+                    dataGridView1.AutoResizeColumns();
+                    dataGridView1.Width = 0;
+                    for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                    {
+
+
+                        dataGridView1.Width += dataGridView1.Columns[i].Width + 10;
+
+                    }
+
+                    if (dataGridView1.Width > 500)
+                        this.Width = dataGridView1.Width + 100;
+                    else
+                        this.Width = 500;
+                    Size resolution = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
+
+                    dataGridView1.Width = this.Width;
+
+
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + " \n " + ex.HResult + " \n " + ex.Source + " \n " + ex.StackTrace + " \n " + ex.HelpLink + " \n " + ex.HelpLink);
+            }
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (dataGridView1.Columns[dataGridView1.CurrentCell.ColumnIndex].HeaderText == "Имя таблицы")
+            {
+                if (sqlConnection == null)
+                    button1.PerformClick();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+
+                sqlCommand.CommandText = "" +
+                      $"SELECT TOP 10 * " +
+                      $"FROM [{dataGridView1.CurrentRow.Cells[0].Value}].[{dataGridView1.CurrentRow.Cells[1].Value}].[{dataGridView1.CurrentCell.Value}]";
+
+                try
+                {
+                    using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                    {
+                        dataGridView2.Rows.Clear();
+                        dataGridView2.Columns.Clear();
+
+                        List<string> data = new List<string>();
+
+                        for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            dataGridView2.Columns.Add(sqlDataReader.GetName(i), sqlDataReader.GetName(i));
+
+
+                        if (sqlDataReader.HasRows)
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                data.Clear();
+                                for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                                {
+                                    if(sqlDataReader.GetValue(i) !=null)
+                                    data.Add(sqlDataReader.GetValue(i).ToString());
+                                }
+                                dataGridView2.Rows.Add(data.ToArray());
+                                
+                            }
+                        }
+                        dataGridView2.AutoResizeColumns();
+                        dataGridView2.Width = 0;
+                        for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                        {
+                            dataGridView2.Width += dataGridView2.Columns[i].Width + 10;
+                        }
+
+                        if (dataGridView2.Width > 500)
+                            this.Width = dataGridView2.Width + 100;
+                        else
+                            this.Width = 500;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + " \n " + ex.HResult + " \n " + ex.Source + " \n " + ex.StackTrace + " \n " + ex.HelpLink + " \n " + ex.Data.ToString());
+                  
+                        foreach (KeyValuePair<string, string> kvp in ex.Data)
+                        {
+                            MessageBox.Show(kvp.Key + "  " + kvp.Value);
+                        }
+                            
+                    
+                }
+            }
+            else
+                MessageBox.Show(dataGridView2.Columns[dataGridView2.CurrentCell.ColumnIndex].HeaderText);
+        }
+
+    }
+}
