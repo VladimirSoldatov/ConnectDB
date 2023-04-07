2628a30fcd912eb9fd3426702cf8d1d1b6b1f6b2
Vladimir <vvsoldatov@rt-dc.ru>
2023-04-07 10:58:24 +0300

    Add requests

--- ConnectDB/MainForm.Designer.cs
+++ ConnectDB/MainForm.Designer.cs
@@ -56,6 +56,7 @@
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -222,7 +223,7 @@
             // button5
             // 
             this.button5.Location = new System.Drawing.Point(566, 392);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(141, 23);
             this.button5.TabIndex = 19;
@@ -240,7 +241,7 @@
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(560, 509);
-            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(141, 25);
             this.button6.TabIndex = 21;
@@ -260,7 +261,8 @@
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Items.AddRange(new object[] {
             "i82z0report01.vats.local",
-            "p0a8i82z2bps02.vats.local"});
+            "p0a8i82z2bps02.vats.local",
+            "172.30.34.7"});
             this.comboBox2.Location = new System.Drawing.Point(269, 555);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(203, 21);
@@ -309,11 +311,22 @@
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(636, 648);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(157, 23);
+            this.button8.TabIndex = 30;
+            this.button8.Text = "Выполнить запрос";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 711);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.textBox9);
@@ -384,6 +397,7 @@
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
